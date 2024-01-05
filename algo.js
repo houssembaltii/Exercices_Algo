b533ce75ffb5f4b9c55eba9412e3cb099d52a2c9
houssembaltii <houssem@bearstudio.fr>
2024-01-05 09:59:50 +0100

    FONCTIONS ET PROCÉDURES

---
+++ algo.js
@@ -0,0 +1,61 @@
+// FONCTIONS ET PROCÉDURES
+// const addition =(a,b)=>{return a+b}
+
+// const soustraction =(a,b)=>{return a-b}
+
+// const multiplication =(a,b)=>{return  a*b}
+
+// const division =(a,b)=>{return parseFloat((a/b).toFixed(2))}
+
+// const algo=(a,b)=>{
+//   console.log("addition",addition(a,b))
+//   console.log("soustraction",soustraction(a,b))
+//   console.log("multiplication",multiplication(a,b))
+//   console.log("division",division(a,b))
+  
+// }
+
+// algo(10,3)
+
+//Bilan
+// //20 battonets
+// const algo=()=>{
+// let n=20;
+// let c1;
+// let turn="joueur1";
+// while(true){
+//   if(turn==="joueur1"){
+//     do{
+//      c1=parseInt(prompt(`joueur1 et le reste est ${n}`))
+//     }
+//     while( (c1>3) || (c1<1) || (n<c1))
+//   	n-=c1
+//     if(n==0)
+//       {
+//      console.log(turn)
+//         alert(`winner is ${turn}`)
+//         break;
+//     }
+//     turn="joueur2"
+//      }
+//   else { 
+//   		do{
+//      c1=parseInt(prompt(`joueur2 et le reste est ${n}`))
+//         }
+//         while( (c1>3) || (c1<1) || (n<c1))
+//   	n-=c1
+//     if(n==0)
+//     { 
+//      console.log(turn)
+//         alert(`winner is ${turn}`)
+
+//       break;
+//     }
+//      turn="joueur1"
+//     }
+// }
+
+// }
+
+
+// algo()

d7c8687b32251fb90436f3a4f786550dadf14049
houssembaltii <houssem@bearstudio.fr>
2024-01-05 09:51:10 +0100

    STRUCTURE CONDITIONNELLE ET STRUCTURE ALTERNATIVE

---
+++ algo.js
@@ -0,0 +1,81 @@
+ //Exercice 1 :
+// const algorithme=(x)=>{
+//   if (x>0){
+//     console.log('ce nombre est superieur a 0')
+//   }
+//   else if (x<0){
+//     console.log('ce nombre est inferieur ')
+//   }
+//   else {
+//     console.log('ce nombre est 0 ! ')
+//   }
+// }
+// // un nombre qui est supp a 0 ne peux pas etre sup ou egale 5>=0
+// algorithme(5)
+// //Exercice2:
+// const algo = (age)=>{
+//  age>=18?console.log("je suis majeur"):console.log("je suis mineur")
+// }
+//Exercice 3:
+// const algorithme = (reponse)=>{
+//   reponse==="non"?console.log("On va tout vous apprendre à M2I Formation "):console.log("Nous n’avons plus rien à vous apprendre")
+// }
+
+// algorithme("non")
+//Exercice 4:
+// const algo= (genre)=>{
+//   if (genre==="fille")
+//     console.log("fille")
+//   else if ( genre==="garçon")
+//     console.log("garçon")
+//   else
+//     console.log("ni garçon ni fille ")
+// }
+// algo("garçon")
+
+//Exercice 5:
+// const algo =(temperature)=>{
+//   if (temperature<=0)
+//     console.log("Etat solide")
+//   else if (temperature>0 && temperature<100){
+//   console.log("Etat liquide")
+//   }
+//   else
+//     console.log("Etat gazeux")
+// }
+// algo(105)
+//Exercice 6 :
+// const algo = (a,b)=>{
+//   if (a===b)
+//     console.log("les nombres sont égaux")
+//   else{
+//     console.log("la difference est : ", a-b)
+//   }
+// }
+// algo(1,2)
+//Exercice 7:
+// var a=5;var b=6;var c=4;
+// const algo = (a,b,c)=>{
+// if(a<=c && a<=b ){
+//   if (b<c){
+//     console.log("l'ordre est ",a,b,c)
+//   }
+//   else
+//     console.log("l'ordre croissant est",a,c,b)
+// }
+//   else if (b<=a && b<=c){
+//     if (a>c)
+//       console.log("l'ordre croissant est ",b,c,a)
+//     else 
+//       console.log("l'odre croissant est ",b,a,c)
+//   }
+//   else {
+//     if (a>b)
+//       console.log("l'ordre croissant est ",c,b,a)
+//      else
+//        console.log("l'odre croissant est ",c,a,b)
+    
+  
+//   }
+// }
+// algo(a,b,c)
